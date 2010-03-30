063fa9d5cc9b8a3f4d8b3b9311267b52eb8a6772
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2010-03-30 07:44:23 +0000

    Fixed building on Windows.

--- source/quakeworld/client/net_wins.c
+++ source/quakeworld/client/net_wins.c
@@ -175,18 +175,18 @@ qboolean NET_GetPacket (void)
 
 	if (ret == -1)
 	{
-		int errno = WSAGetLastError();
+		int err = WSAGetLastError();
 
-		if (errno == WSAEWOULDBLOCK)
+		if (err == WSAEWOULDBLOCK)
 			return false;
-		if (errno == WSAEMSGSIZE) {
+		if (err == WSAEMSGSIZE) {
 			Con_Printf ("Warning:  Oversize packet from %s\n",
 				NET_AdrToString (net_from));
 			return false;
 		}
 
 
-		Sys_Error ("NET_GetPacket: %s", strerror(errno));
+		Sys_Error ("NET_GetPacket: %s", strerror(err));
 	}
 
 	net_message.cursize = ret;
