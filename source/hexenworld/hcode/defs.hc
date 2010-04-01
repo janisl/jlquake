8c51312dee7c9f46de101f167adb1c959aa5bcd1
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2010-04-01 10:14:57 +0000

    Removed HexenWorld game sources.

--- source/hexenworld/hcode/defs.hc
+++
@@ -1,5 +0,0 @@
-/*
- * $Header: /HexenWorld/HCode/Defs.hc 1     2/04/98 1:59p Rjohnson $
- */
-
-// !!! Not used !!!
--- source/hexenworld/hcode/hc.bat
+++
@@ -1,6 +0,0 @@
-@hccmp -oi -on -quiet
-@xcopy/r progs.dat ..\hw > nul
-@attrib +r ..\hw\progs.dat
-@xcopy/r files.dat ..\hw > nul
-@attrib +r ..\hw\files.dat
-
