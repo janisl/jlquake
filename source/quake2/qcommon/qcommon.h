6b48af984c27807e7f7a8e76a09d48f73079890e
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2010-04-02 08:10:27 +0000

    Copied files that Quake 2 shared with game so that engine copies can be converted to C++.

--- source/quake2/qcommon/qcommon.h
+++ source/quake2/qcommon/qcommon.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // qcommon.h -- definitions common between client and server, but not game.dll
 
-#include "../game/q_shared.h"
+#include "q_shared.h"
 
 
 #define	VERSION		3.19
