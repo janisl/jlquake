d4432a08eba7d142751805407dff10b1e2585f78
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2010-04-11 20:36:32 +0000

    Fix.

--- source/hexenworld/server/sv_main.cpp
+++ source/hexenworld/server/sv_main.cpp
@@ -1730,7 +1730,7 @@ void SV_Init (quakeparms_t *parms)
 	Con_Printf ("======== HexenWorld Initialized ========\n");
 	
 // process command line arguments
-	Cmd_StuffCmds_f ();
+	Cbuf_ExecuteText(EXEC_NOW, "stuffcmds");
 	Cbuf_Execute ();
 
 // if a map wasn't specified on the command line, spawn start.map
--- source/quakeworld/server/sv_main.cpp
+++ source/quakeworld/server/sv_main.cpp
@@ -1669,7 +1669,7 @@ void SV_Init (quakeparms_t *parms)
 	Con_Printf ("======== QuakeWorld Initialized ========\n");
 	
 // process command line arguments
-	Cmd_StuffCmds_f ();
+	Cbuf_ExecuteText(EXEC_NOW, "stuffcmds");
 	Cbuf_Execute ();
 
 // if a map wasn't specified on the command line, spawn start.map
