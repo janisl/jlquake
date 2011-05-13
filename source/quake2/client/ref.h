cceed6dcc5ed713048db2904493f66b46ca4ba05
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2011-05-14 00:40:25 +0300

    Removed unused field.

--- source/quake2/client/ref.h
+++ source/quake2/client/ref.h
@@ -49,7 +49,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 struct entity_t : refEntity_base_t
 {
-	int		lightstyle;				// for flashing entities
 	float	alpha;					// ignore if RF_TRANSLUCENT isn't set
 
 	struct image_t	*skin;			// NULL for inline skin
