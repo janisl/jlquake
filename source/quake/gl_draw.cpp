b7ed4b57bef6d99112f169d4432ef8853754b491
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2011-07-27 21:36:27 +0300

    Removed unneeded header file, cleaned up an unused constant.

--- source/quake/gl_draw.cpp
+++ source/quake/gl_draw.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 
-#define GL_COLOR_INDEX8_EXT     0x80E5
-
 image_t*	draw_backtile;
 
 image_t*	char_texture;
--- source/quake2/ref_gl/gl_local.h
+++ source/quake2/ref_gl/gl_local.h
@@ -28,13 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <GL/glu.h>
 
-#ifndef GL_COLOR_INDEX8_EXT
-#define GL_COLOR_INDEX8_EXT GL_COLOR_INDEX
-#endif
-
 //===================================================================
-
-#include "gl_model.h"
 
 extern	QCvar	*gl_polyblend;
 
