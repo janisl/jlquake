5a1b3b33e760daaf36c45ace6304aa22c07aa2b4
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2011-08-06 00:47:05 +0300

    Removed dependency on renderer's header.

--- source/quake2/client/cl_scrn.cpp
+++ source/quake2/client/cl_scrn.cpp
@@ -33,7 +33,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
   */
 
 #include "client.h"
-#include "../../client/render_local.h"
 
 float		scr_con_current;	// aproaches scr_conlines at scr_conspeed
 float		scr_conlines;		// 0.0 to 1.0 lines of console to display
@@ -1243,7 +1242,7 @@ void SCR_UpdateScreen (void)
 	else if ( cl_stereo_separation->value < 0 )
 		Cvar_SetValueLatched( "cl_stereo_separation", 0.0 );
 
-	if (glConfig.stereoEnabled)
+	if (cls.glconfig.stereoEnabled)
 	{
 		SCR_DrawScreen(STEREO_LEFT, -cl_stereo_separation->value / 2);
 		SCR_DrawScreen(STEREO_RIGHT, cl_stereo_separation->value / 2);
