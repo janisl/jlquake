5c0a5f9ebe9cee4aa066cac5f15e31c5eb3e586a
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2011-08-06 15:07:52 +0300

    Merged gl_ngraph into gl_draw.

--- source/quakeworld/client/gl_draw.cpp
+++ source/quakeworld/client/gl_draw.cpp
@@ -163,3 +163,75 @@ void Draw_FadeScreen (void)
 {
 	UI_Fill(0 ,0, viddef.width, viddef.height, 0, 0, 0, 0.8);
 }
+
+#define NET_GRAPHHEIGHT 32
+
+static void R_LineGraph(int x, int y, int h)
+{
+	int r, g, b;
+	if (h == 10000)
+	{
+		// yellow
+		r = 1;
+		g = 1;
+		b = 0;
+	}
+	else if (h == 9999)
+	{
+		// red
+		r = 1;
+		g = 0;
+		b = 0;
+	}
+	else if (h == 9998)
+	{
+		// blue
+		r = 0;
+		g = 0;
+		b = 1;
+	}
+	else
+	{
+		// white
+		r = 1;
+		g = 1;
+		b = 1;
+	}
+
+	if (h > NET_GRAPHHEIGHT)
+	{
+		h = NET_GRAPHHEIGHT;
+	}
+
+	UI_Fill(8 + x, y + NET_GRAPHHEIGHT - h, 1, h, r, g, b, 1);
+}
+
+/*
+==============
+R_NetGraph
+==============
+*/
+void R_NetGraph (void)
+{
+	int		x, y;
+	char st[80];
+
+	x = 0;
+	int lost = CL_CalcNet();
+
+	x =	-((viddef.width - 320)>>1);
+	y = viddef.height - sb_lines - 24 - NET_GRAPHHEIGHT - 1;
+
+	M_DrawTextBox (x, y, NET_TIMINGS/8, NET_GRAPHHEIGHT/8 + 1);
+	y += 8;
+
+	sprintf(st, "%3i%% packet loss", lost);
+	Draw_String(8, y, st);
+	y += 8;
+
+	for (int a = 0; a < NET_TIMINGS; a++)
+	{
+		int i = (cls.netchan.outgoing_sequence - a) & NET_TIMINGSMASK;
+		R_LineGraph(NET_TIMINGS - 1 - a, y, packet_latency[i]);
+	}
+}
