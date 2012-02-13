c21419b132d131f8ebf93ac105866bebc8f80fd2
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-02-13 21:56:17 +0200

    Changed licence to GPL version 3 because that's what Wolfenstein and Doom 3 code uses.

--- source/core/md4.cpp
+++ source/core/md4.cpp
@@ -4,7 +4,7 @@
 //**
 //**	This program is free software; you can redistribute it and/or
 //**  modify it under the terms of the GNU General Public License
-//**  as published by the Free Software Foundation; either version 2
+//**  as published by the Free Software Foundation; either version 3
 //**  of the License, or (at your option) any later version.
 //**
 //**	This program is distributed in the hope that it will be useful,
@@ -34,9 +34,9 @@ typedef quint32 UINT4;
 
 All rights reserved.
   
-License to copy and use this software is granted provided that it is identified as the “RSA Data Security, Inc. MD4 Message-Digest Algorithm” in all material mentioning or referencing this software or this function.
-License is also granted to make and use derivative works provided that such works are identified as “derived from the RSA Data Security, Inc. MD4 Message-Digest Algorithm” in all material mentioning or referencing the derived work.
-RSA Data Security, Inc. makes no representations concerning either the merchantability of this software or the suitability of this software for any particular purpose. It is provided “as is” without express or implied warranty of any kind.
+License to copy and use this software is granted provided that it is identified as the ï¿½RSA Data Security, Inc. MD4 Message-Digest Algorithmï¿½ in all material mentioning or referencing this software or this function.
+License is also granted to make and use derivative works provided that such works are identified as ï¿½derived from the RSA Data Security, Inc. MD4 Message-Digest Algorithmï¿½ in all material mentioning or referencing the derived work.
+RSA Data Security, Inc. makes no representations concerning either the merchantability of this software or the suitability of this software for any particular purpose. It is provided ï¿½as isï¿½ without express or implied warranty of any kind.
   
 These notices must be retained in any copies of any part of this documentation and/or software. */
 
