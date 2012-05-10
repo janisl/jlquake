3e49dcca2d280afb954ce28650bd2963b8631f31
Jānis Legzdiņš <legzdinsjanis@gmail.com>
2012-05-10 20:46:55 +0300

    Merged initialisation of script punctuations.

--- source/common/script.cpp
+++ source/common/script.cpp
@@ -15,3 +15,140 @@
 //**************************************************************************
 
 #include "qcommon.h"
+
+//longer punctuations first
+static punctuation_t default_punctuations[] =
+{
+	//binary operators
+	{">>=",P_RSHIFT_ASSIGN, NULL},
+	{"<<=",P_LSHIFT_ASSIGN, NULL},
+	//
+	{"...",P_PARMS, NULL},
+	//define merge operator
+	{"##",P_PRECOMPMERGE, NULL},
+	//logic operators
+	{"&&",P_LOGIC_AND, NULL},
+	{"||",P_LOGIC_OR, NULL},
+	{">=",P_LOGIC_GEQ, NULL},
+	{"<=",P_LOGIC_LEQ, NULL},
+	{"==",P_LOGIC_EQ, NULL},
+	{"!=",P_LOGIC_UNEQ, NULL},
+	//arithmatic operators
+	{"*=",P_MUL_ASSIGN, NULL},
+	{"/=",P_DIV_ASSIGN, NULL},
+	{"%=",P_MOD_ASSIGN, NULL},
+	{"+=",P_ADD_ASSIGN, NULL},
+	{"-=",P_SUB_ASSIGN, NULL},
+	{"++",P_INC, NULL},
+	{"--",P_DEC, NULL},
+	//binary operators
+	{"&=",P_BIN_AND_ASSIGN, NULL},
+	{"|=",P_BIN_OR_ASSIGN, NULL},
+	{"^=",P_BIN_XOR_ASSIGN, NULL},
+	{">>",P_RSHIFT, NULL},
+	{"<<",P_LSHIFT, NULL},
+	//reference operators
+	{"->",P_POINTERREF, NULL},
+	//C++
+	{"::",P_CPP1, NULL},
+	{".*",P_CPP2, NULL},
+	//arithmatic operators
+	{"*",P_MUL, NULL},
+	{"/",P_DIV, NULL},
+	{"%",P_MOD, NULL},
+	{"+",P_ADD, NULL},
+	{"-",P_SUB, NULL},
+	{"=",P_ASSIGN, NULL},
+	//binary operators
+	{"&",P_BIN_AND, NULL},
+	{"|",P_BIN_OR, NULL},
+	{"^",P_BIN_XOR, NULL},
+	{"~",P_BIN_NOT, NULL},
+	//logic operators
+	{"!",P_LOGIC_NOT, NULL},
+	{">",P_LOGIC_GREATER, NULL},
+	{"<",P_LOGIC_LESS, NULL},
+	//reference operator
+	{".",P_REF, NULL},
+	//seperators
+	{",",P_COMMA, NULL},
+	{";",P_SEMICOLON, NULL},
+	//label indication
+	{":",P_COLON, NULL},
+	//if statement
+	{"?",P_QUESTIONMARK, NULL},
+	//embracements
+	{"(",P_PARENTHESESOPEN, NULL},
+	{")",P_PARENTHESESCLOSE, NULL},
+	{"{",P_BRACEOPEN, NULL},
+	{"}",P_BRACECLOSE, NULL},
+	{"[",P_SQBRACKETOPEN, NULL},
+	{"]",P_SQBRACKETCLOSE, NULL},
+	//
+	{"\\",P_BACKSLASH, NULL},
+	//precompiler operator
+	{"#",P_PRECOMP, NULL},
+	{"$",P_DOLLAR, NULL},
+	{NULL, 0}
+};
+
+static void PS_CreatePunctuationTable(script_t* script, punctuation_t* punctuations)
+{
+	//get memory for the table
+	if (!script->punctuationtable)
+	{
+		script->punctuationtable = (punctuation_t**)Mem_Alloc(256 * sizeof(punctuation_t*));
+	}
+	Com_Memset(script->punctuationtable, 0, 256 * sizeof(punctuation_t*));
+	//add the punctuations in the list to the punctuation table
+	for (int i = 0; punctuations[i].p; i++)
+	{
+		punctuation_t* newp = &punctuations[i];
+		punctuation_t* lastp = NULL;
+		//sort the punctuations in this table entry on length (longer punctuations first)
+		punctuation_t* p;
+		for (p = script->punctuationtable[(unsigned int)newp->p[0]]; p; p = p->next)
+		{
+			if (String::Length(p->p) < String::Length(newp->p))
+			{
+				newp->next = p;
+				if (lastp)
+				{
+					lastp->next = newp;
+				}
+				else
+				{
+					script->punctuationtable[(unsigned int)newp->p[0]] = newp;
+				}
+				break;
+			}
+			lastp = p;
+		}
+		if (!p)
+		{
+			newp->next = NULL;
+			if (lastp)
+			{
+				lastp->next = newp;
+			}
+			else
+			{
+				script->punctuationtable[(unsigned int)newp->p[0]] = newp;
+			}
+		}
+	}
+}
+
+void SetScriptPunctuations(script_t* script, punctuation_t* p)
+{
+	if (p)
+	{
+		PS_CreatePunctuationTable(script, p);
+		script->punctuations = p;
+	}
+	else
+	{
+		PS_CreatePunctuationTable(script, default_punctuations);
+		script->punctuations = default_punctuations;
+	}
+}
--- source/enemy_territory/botlib/l_script.h
+++ source/enemy_territory/botlib/l_script.h
@@ -80,69 +80,6 @@ If you have questions concerning this license or the applicable additional terms
 #define TT_INTEGER                  0x1000	// integer number
 #define TT_LONG                     0x2000	// long number
 #define TT_UNSIGNED                 0x4000	// unsigned number
-//punctuation sub type
-//--------------------
-#define P_RSHIFT_ASSIGN             1
-#define P_LSHIFT_ASSIGN             2
-#define P_PARMS                     3
-#define P_PRECOMPMERGE              4
-
-#define P_LOGIC_AND                 5
-#define P_LOGIC_OR                  6
-#define P_LOGIC_GEQ                 7
-#define P_LOGIC_LEQ                 8
-#define P_LOGIC_EQ                  9
-#define P_LOGIC_UNEQ                    10
-
-#define P_MUL_ASSIGN                    11
-#define P_DIV_ASSIGN                    12
-#define P_MOD_ASSIGN                    13
-#define P_ADD_ASSIGN                    14
-#define P_SUB_ASSIGN                    15
-#define P_INC                           16
-#define P_DEC                           17
-
-#define P_BIN_AND_ASSIGN            18
-#define P_BIN_OR_ASSIGN             19
-#define P_BIN_XOR_ASSIGN            20
-#define P_RSHIFT                        21
-#define P_LSHIFT                        22
-
-#define P_POINTERREF                    23
-#define P_CPP1                          24
-#define P_CPP2                          25
-#define P_MUL                           26
-#define P_DIV                           27
-#define P_MOD                           28
-#define P_ADD                           29
-#define P_SUB                           30
-#define P_ASSIGN                        31
-
-#define P_BIN_AND                       32
-#define P_BIN_OR                        33
-#define P_BIN_XOR                       34
-#define P_BIN_NOT                       35
-
-#define P_LOGIC_NOT                 36
-#define P_LOGIC_GREATER             37
-#define P_LOGIC_LESS                    38
-
-#define P_REF                           39
-#define P_COMMA                     40
-#define P_SEMICOLON                 41
-#define P_COLON                     42
-#define P_QUESTIONMARK              43
-
-#define P_PARENTHESESOPEN           44
-#define P_PARENTHESESCLOSE          45
-#define P_BRACEOPEN                 46
-#define P_BRACECLOSE                    47
-#define P_SQBRACKETOPEN             48
-#define P_SQBRACKETCLOSE            49
-#define P_BACKSLASH                 50
-
-#define P_PRECOMP                       51
-#define P_DOLLAR                        52
 //name sub type
 //-------------
 //		the length of the name
@@ -175,8 +112,6 @@ void StripSingleQuotes(char* string);
 signed long int ReadSignedInt(script_t* script);
 //read a possible signed floating point number
 long double ReadSignedFloat(script_t* script);
-//set an array with punctuations, NULL restores default C/C++ set
-void SetScriptPunctuations(script_t* script, punctuation_t* p);
 //set script flags
 void SetScriptFlags(script_t* script, int flags);
 //get script flags
